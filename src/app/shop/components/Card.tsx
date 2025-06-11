4e52e72188e4998f9ce07f13b2944ccf0006abd7
Briamco <crossinerobriamco@gmail.com>
2025-06-11 19:20:49 -0400

    Refactor Card component for improved layout; enhance Page component's responsiveness and styling

--- src/app/shop/components/Card.tsx
+++ src/app/shop/components/Card.tsx
@@ -5,8 +5,8 @@ import { redirect } from "next/navigation";
 
 export default function Card() {
   return (
-    <article onClick={() => redirect('/shop/1')} className="w-[280px] h-fit bg-white shadow-2xl rounded-[10px] flex flex-col items-center justify-between transition-all duration-300 hover:scale-105 cursor-pointer">
-      <div className="w-full h-[126px] bg-gray-400 rounded-t-[10px]"></div>
+    <article onClick={() => redirect('/shop/1')} className="w-full md:max-w-[280px] h-fit bg-white shadow-2xl rounded-[10px] flex flex-col items-center justify-between transition-all duration-300 hover:scale-105 cursor-pointer">
+      <div className="w-full aspect-video bg-gray-400 rounded-t-[10px]"></div>
       <div className="flex items-center justify-between w-full px-4 py-1">
         <div>
           <h1>Lorem Ipsum</h1>
--- src/app/shop/layout.tsx
+++ src/app/shop/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Cart from "./components/Cart";
 
 export default function Layout({
@@ -6,10 +7,16 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex space-x-4 justify-between">
+    <div className="flex flex-col lg:flex-row md:space-x-4 justify-between">
       <main className="flex-1">{children}</main>
       <div className="w-auto">
-        <div className="sticky top-8">
+        <Link
+          href='/cart'
+          className="btn w-full left-0 right-2 bottom-3 z-50 fixed text-center text-4xl rounded-none rounded-t-[10px] lg:hidden"
+        >
+          Ir al carro
+        </Link>
+        <div className="hidden lg:block sticky top-8">
           <Cart />
         </div>
       </div>
